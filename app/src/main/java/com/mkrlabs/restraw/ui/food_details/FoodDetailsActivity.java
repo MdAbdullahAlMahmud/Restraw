f858704697668c50fa67e18d8c1b9c7324e1c9a5
MdAbdullahAlMahmud <mmunna864@gmail.com>
2022-03-26 00:49:48 +0600

    food description screen added

--- app/src/main/java/com/mkrlabs/restraw/ui/food_details/FoodDetailsActivity.java
+++ app/src/main/java/com/mkrlabs/restraw/ui/food_details/FoodDetailsActivity.java
@@ -1,16 +1,31 @@
 package com.mkrlabs.restraw.ui.food_details;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
+import androidx.appcompat.widget.AppCompatImageButton;
+import androidx.cardview.widget.CardView;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.mkrlabs.restraw.R;
 
 public class FoodDetailsActivity extends AppCompatActivity {
 
+    AppCompatImageButton backButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_food_details);
+        init();
+
+
+        backButton.setOnClickListener(view -> onBackPressed());
+    }
+
+
+
+    private  void  init(){
+        backButton = findViewById(R.id.backButton);
     }
 }
