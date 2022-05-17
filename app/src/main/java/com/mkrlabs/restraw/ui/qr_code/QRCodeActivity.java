07691db44652ef2bbd7cc78d1341a370e5acedf4
MdAbdullahAlMahmud <mmunna864@gmail.com>
2022-05-18 01:36:16 +0600

    QR code feature integreted

--- app/src/main/java/com/mkrlabs/restraw/ui/qr_code/QRCodeActivity.java
+++ app/src/main/java/com/mkrlabs/restraw/ui/qr_code/QRCodeActivity.java
@@ -5,15 +5,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import com.google.android.material.button.MaterialButton;
 import com.mkrlabs.customstatusbar.CustomStatusBar;
 import com.mkrlabs.restraw.R;
 import com.mkrlabs.restraw.ui.HomeActivity;
+import com.mkrlabs.restraw.utils.QRScanner;
 
 public class QRCodeActivity extends AppCompatActivity {
 
-    MaterialButton exploreRestrawButton;
+    public static  TextView qrTextTV;
+
+    MaterialButton exploreRestrawButton,scanQrCode;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,7 +30,16 @@ public class QRCodeActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Intent intent = new Intent(QRCodeActivity.this, HomeActivity.class);
                 startActivity(intent);
-                finish();
+               finish();
+
+            }
+        });
+        scanQrCode.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(QRCodeActivity.this, QRScanner.class);
+                startActivity(intent);
+               // finish();
 
             }
         });
@@ -34,6 +47,8 @@ public class QRCodeActivity extends AppCompatActivity {
     }
 
     private void inits() {
+        scanQrCode= findViewById(R.id.scanQrCode);
+        qrTextTV= findViewById(R.id.qrText);
         exploreRestrawButton= findViewById(R.id.exploreRestrawButton);
     }
 }
