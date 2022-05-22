b8599453ea3f7ba26372fadf0fc21a4fbea2ad49
MdAbdullahAlMahmud <mmunna864@gmail.com>
2022-05-22 09:29:02 +0600

    Try list activity added

---
+++ app/src/main/java/com/mkrlabs/restraw/ui/try_list/TryListActivity.java
@@ -0,0 +1,14 @@
+package com.mkrlabs.restraw.ui.try_list;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TryListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_try_list);
+    }
+}
